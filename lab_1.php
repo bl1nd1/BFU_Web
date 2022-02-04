e80f754038ca1b465cdaf7fd1fba2ca4bedd04ff
PopovAlex <c76167970@gmail.com>
2022-02-04 21:50:59 +0200

    lab_1.php: сделан пункт 2

--- lab_1.php
+++ lab_1.php
@@ -9,3 +9,25 @@ $order = &$very_bad_unclear_name;
 $order .= " with the spicy sauce";
 
 echo "\nYour order is: $very_bad_unclear_name.\n";
+
+echo "\n2. Числа:\n";
+
+$a = 123;
+
+echo $a . "\n";
+
+$b = 321;
+
+echo $b . "\n";
+
+$c = 0.123;
+
+echo $c . "\n";
+
+echo 12 . "\n";
+
+$last_month = 1187.23;
+
+$this_month = 1089.98;
+
+echo ($last_month - $this_month) . "\n";
