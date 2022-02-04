f67c182c707698b9367560225dc48b7c545fcd06
PopovAlex <c76167970@gmail.com>
2022-02-04 21:52:49 +0200

    lab_1.php: сделан пункт 14

--- lab_1.php
+++ lab_1.php
@@ -65,3 +65,126 @@ $answer /= 2;
 $answer -= $my_num;
 
 echo $answer . "\n";
+
+echo "\n14. Математические функции:\n";
+
+echo "\nРабота с %:\n";
+
+$a = 10;
+
+$b = 3;
+
+echo $a % $b . "\n";
+
+if ($a % $b === 0)
+{
+	echo "Делится:" . $a / $b . "\n";
+}
+else
+{
+	echo "Делится с остатком: " . $a % $b . "\n";
+}
+
+echo "\nРабота со степенью и корнем:\n";
+
+$st = pow(2, 10);
+
+echo $st . "\n";
+
+echo sqrt(245) . "\n";
+
+$array = [4, 2, 5, 19, 13, 0, 10];
+
+$sum = 0;
+
+foreach ($array as $num)
+{
+	$sum += $num ** 2;
+}
+
+echo sqrt($sum) . "\n";
+
+echo "\nРабота с функциями округления:\n";
+
+echo round(sqrt(379), 1) . "\n";
+
+echo round(sqrt(379), 2) . "\n";
+
+echo round(sqrt(379), 3) . "\n";
+
+$array = [];
+
+$array = [
+	'floor' => floor(sqrt(587)),
+	'ceil' => ceil(sqrt(587))
+];
+
+echo "\nРабота с min и max:\n";
+
+$array = [4, -2, 5, 19, -130, 0, 10];
+
+echo max($array) . "\n";
+
+echo min($array) . "\n";
+
+echo "\nРабота с рандомом:\n";
+
+echo rand(1, 100) . "\n";
+
+$array = [];
+
+for ($i = 0; $i < 10; $i++)
+{
+	$array = rand(1, 100);
+}
+
+echo "\nРабота с модулем:\n";
+
+echo abs($a - $b) . "\n";
+
+$a = 1;
+
+$b = 10;
+
+echo abs($a - $b) . "\n";
+
+$array = [];
+
+$array = [1, 2, -1, -2, 3, -3];
+
+$new_array = [];
+
+foreach ($array as $num)
+{
+	array_push($new_array, abs($num));
+}
+
+echo "\nОбщее:\n";
+
+$a = 30;
+
+$del = [];
+
+for ($i = $a; $i > 0; $i--)
+{
+	if ($a % $i === 0)
+	{
+		array_push($del, $i);
+	}
+}
+
+$array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+$sum = 0;
+
+for ($i = 0; $i < sizeof($array); $i++)
+{
+	$sum += $array[$i];
+
+	if ($sum > 10)
+	{
+		echo $i + 1 . "\n";
+
+		break;
+	}
+}
