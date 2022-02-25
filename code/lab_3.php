99fec4c621e7f7525cf1ed1d6921df4665020250
PopovAlex <c76167970@gmail.com>
2022-02-25 21:10:12 +0200

    lab_3.php: finished 1 task

---
+++ code/lab_3.php
@@ -0,0 +1,17 @@
+<?php
+
+echo "1(a) task\n";
+$regex = "/a..b/";
+$str = explode(' ', "ahb acb aeb aeeb adcb axeb");
+foreach ($str as $word)
+{
+	if (preg_match($regex, $word))
+	{
+		echo $word . "\n";
+	}
+}
+
+echo "1(b) task\n";
+$regex = "/(\d)+/";
+$str = "a1b2c3";
+echo preg_replace_callback($regex, function($matches) { return intval($matches[0]) ** 3; }, $str);
