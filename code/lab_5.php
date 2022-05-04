bf1b06312c132e02ff8fbde58dbb228824747372
PopovAlex <c76167970@gmail.com>
2022-05-04 17:34:08 +0200

    lab_5 done

--- code/lab_5.php
+++ code/lab_5.php
@@ -8,15 +8,59 @@ if (mysqli_connect_errno())
 	exit;
 }
 
-$mysqli->query('INSERT INTO ad (email, title, description, category) VALUES ("test@test.com", "title", "desc", "other")');
-if ($result = $mysqli->query('SELECT * FROM ad ORDER BY created DESC'))
+if ($_SERVER['REQUEST_METHOD'] === 'POST')
 {
-	print("Ads:\n");
-	while ($row = $result->fetch_assoc())
-	{
-		printf("%s (%s)\n", $row['email'], $row['title']);
-	}
-	$result->close();
+	var_dump($_POST);
+	$query = "INSERT INTO ad(email, title, description, category) 
+				VALUES('{$_POST["email"]}', '{$_POST["title"]}', '{$_POST["desc"]}', '{$_POST["category"]}')";
+	$mysqli->query($query);
 }
 
-$mysqli->close();
+$result = $mysqli->query('SELECT * FROM ad ORDER BY created DESC');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Title</title>
+</head>
+<body>
+<table>
+	<tr>
+		<th>Category</th>
+		<th>Email</th>
+		<th>Title</th>
+	</tr>
+	<?php foreach ($result as $row)
+	{
+		echo "	<tr>
+			<td>{$row['category']}</td>
+			<td>{$row['email']}</td>
+			<td>{$row['title']}</td>
+		</tr>";
+	} ?>
+</table>
+<form name="adv" action="lab_5.php" method="POST">
+	<label>
+		<input type="email" name="email" placeholder="Email..." />
+	</label>
+
+	<label>
+		<select name="category">
+			<option selected disabled>Category...</option>
+			<option>category1</option>
+			<option>category2</option>
+			<option>category3</option>
+		</select>
+	</label>
+	<label>
+		<input type="text" name="title" placeholder="Title..." />
+	</label>
+	<label>
+		<textarea name="desc" placeholder="Description..."></textarea>
+	</label>
+	<input type="submit" value="Add" />
+</form>
+</body>
+</html>
